0d08866dd649eea815c7226b4f7d262339ed645e
Borland <aborland@tubecityims.com>
2017-06-09 18:45:54 -0400

    wrote solving function

--- countdown_gameshow.py
+++ countdown_gameshow.py
@@ -10,6 +10,8 @@
 # challenge input:
 # 25 100 9 7 3 7 881 -> 7 * 3 + 100 * 7 + 25 + 9 = 881
 # 6 75 3 25 50 100 952 -> 100 + 6 * 3 * 75 - 50 / 25 = 952
+import itertools
+
 def get_input():
     string_of_numbers = input("> ")
     list_of_numbers = [int(i) for i in string_of_numbers.split(" ")]
@@ -18,6 +20,8 @@ def get_input():
 def eval_single(x1, o1, x2):
     valid_operators = ["+", "-", "*", "/"]
     assert o1 in valid_operators
+    if x2 == 0 and o1 == "/":
+        return None
     if o1 == "+":
         return x1 + x2
     elif o1 == "-":
@@ -32,19 +36,56 @@ def eval_single(x1, o1, x2):
 
 def eval_combination(x1, o1, x2, o2, x3, o3, x4, o4, x5, o5, x6):
     valid_operators = ["+", "-", "*", "/"]
-    assert set(x1, x2, x3, x4, x5, x6) in set(valid_operators)
-    
+    for o in [o1, o2, o3, o4, o5]:
+        assert o in set(valid_operators)
+    result = eval_single(x1, o1, x2)
+    result = eval_single(result, o2, x3)
+    result = eval_single(result, o3, x4)
+    result = eval_single(result, o4, x5)
+    retult = eval_single(result, o5, x6)
+    return result
+
+def mix_operators():
+    operators = ["+", "-", "*", "/"]
+    result = []
+    for i in operators:
+        for j in operators:
+            for k in operators:
+                for m in operators:
+                    for n in operators:
+                        result.append([i, j, k, m, n])
+    return result
+
+def find_solution(x1, x2, x3, x4, x5, x6, target):
+    operands = [x1, x2, x3, x4, x5, x6]
+    for i in itertools.permutations(operands):
+        for j in mix_operators():
+            print(i[0], j[0], i[1], j[1], i[2], j[2], i[3], j[3], i[4], j[4], i[5])
+            if eval_combination(i[0], j[0], i[1], j[1], i[2], j[2], i[3], j[3], i[4], j[4], i[5]) == target:
+                return {"operands": i, "operators": j}
 
 while True:
     try:
         numbers = get_input()
-        operands = numbers[0:5]
+        operands = numbers[0:6]
         target = numbers[6]
         break
     except:
         print("Please enter exactly 7 numbers.")
 
-print("-: ", eval_single(operands[0],"-",operands[1]))
-print("+: ", eval_single(operands[0],"+",operands[1]))
-print("*: ", eval_single(operands[0],"*",operands[1]))
-print("/: ", eval_single(operands[0],"/",operands[1]))
+print(f"Operands: {operands}")
+print(f"Target: {target}")
+
+solution = find_solution(operands[0], operands[1], operands[2], operands[3],
+operands[4], operands[5], target)
+
+out_string = ""
+for i in range(5):
+    out_string += str(solution['operands'][i])
+    out_string += " "
+    out_string += solution['operators'][i]
+    out_string += " "
+out_string += str(solution['operands'][5])
+out_string += " = " + str(target)
+
+print(out_string)
