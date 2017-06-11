4dbdea717e09daf2f470acb94edb339a23e12616
Borland <aborland@tubecityims.com>
2017-06-11 11:49:59 -0400

    Finished knight thing

---
+++ knight_path.py
@@ -0,0 +1,42 @@
+# on a grid extending infinitely in ever direction,
+# find the number a moves a knight takes to get to (x, y) from (0, 0)
+# print a path of that length
+
+def get_input():
+    x, y = input("> ").split(" ")
+    return [int(x), int(y)]
+
+def possible_jumps(path):
+    x, y = path[-1]
+    return [
+        path + [[x + 1, y + 2]],
+        path + [[x - 1, y + 2]],
+        path + [[x - 1, y - 2]],
+        path + [[x + 1, y - 2]],
+        path + [[x + 2, y + 1]],
+        path + [[x + 2, y - 1]],
+        path + [[x - 2, y + 1]],
+        path + [[x - 2, y - 1]]
+    ]
+
+def checking_function(paths, dest):
+    for path in paths:
+        if path[-1] == dest:
+            return path
+    return False
+
+def find_path(c_orig, c_dest):
+    current_paths = [[c_orig]]
+    while not checking_function(current_paths, c_dest):
+        new_paths = []
+        for path in current_paths:
+            new_paths += possible_jumps(path)
+        current_paths += new_paths
+    return checking_function(current_paths, c_dest)
+
+coords = get_input()
+
+solution = find_path([0, 0], coords)
+
+print(f"Found a path of length {len(solution)}:")
+print(solution)
